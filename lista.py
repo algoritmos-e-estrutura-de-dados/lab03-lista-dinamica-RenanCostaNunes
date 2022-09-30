b7c4be69ce915e21e6b5504ba5062caafae25cd1
Renan da Costa Nunes <64923516+RenanCostaNunes@users.noreply.github.com>
2022-09-30 01:20:19 -0300

    Commit Lab 03

---
+++ lista.py
@@ -0,0 +1,48 @@
+from node import Node
+
+
+class Lista:
+    inicio = None
+
+    def __init__(self):
+        self.inicio = None
+
+        # adicionar no final da lista
+    def adicionar(self, valor, inicio = False):
+        if(inicio):
+            self.adicionar_no_inicio(valor)
+        else:
+            self.adicionar_no_fim(valor)
+
+  
+    def adiciona_inicio(self, valor):
+        print('inserindo no inicio')
+      
+      
+    def adicionar_fim(self, valor):
+        if (self.inicio == None):
+            self.inicio = Node(valor, None)
+        else:
+            aux = self.inicio
+            while (aux.proximo != None):
+                aux = aux.proximo
+
+            aux.proximo = Node(valor, None)
+
+  
+    def remove(self, valor):
+        anterior = None
+        aux = self.inicio
+
+        
+      
+  
+    def mostra(self):
+        aux = self.inicio
+        print("[", end='')
+        while (aux.proximo != None):
+            print(f"{aux.valor}", end=', ')
+            aux = aux.proximo
+        print(f"{aux.valor}]")
+
+    
